d737ebd45592cb0b28cee1fa6bd8585e913eeb91
Tahseen K. Kakar <tahseen.khan.kakar@gmail.com>
2024-12-21 04:32:43 +0500

    Temporarily comment out authentication in DashboardController and add logging in SessionsController
    - Commented out the require_authentication before_action in DashboardController for testing purposes.
    - Added logging statements in the SessionsController to track user authentication, session creation, and redirection process.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class DashboardController < ApplicationController
-  before_action :require_authentication
+  # Comment this out temporarily for testing
+  # before_action :require_authentication
 
   def show
+    Rails.logger.info "Dashboard#show accessed"  # Add logging
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,19 @@ class SessionsController < ApplicationController
 
   def create
     if user = User.authenticate_by(email_address: params[:email_address], password: params[:password])
+      Rails.logger.info "User authenticated successfully: #{user.id}"
+
       session = user.sessions.create!(
         ip_address: request.ip,
         user_agent: request.user_agent
       )
+      Rails.logger.info "Session created: #{session.id}"
 
       cookies.signed.permanent[:session_token] = { value: session.id, httponly: true }
+      Rails.logger.info "About to redirect to: #{app_path}"
 
-      redirect_to app_path
+      redirect_to app_path and return
+      Rails.logger.info "This line shouldn't be reached"
     else
       redirect_to new_session_path, alert: "Invalid email or password"
     end
