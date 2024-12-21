66eace7a3acbed40e46228c90afcc929b4dfd020
Tahseen K. Kakar <tahseen.khan.kakar@gmail.com>
2024-12-21 22:53:47 +0500

    Refactor routes for tables and improve URL structure
    - Updated routes for tables to use cleaner URLs, enhancing readability and organization.
    - Introduced a new `switch` route for table switching functionality, improving user interaction.
    - Scoped routes for game formats, players, and sessions under the current table, streamlining access and management.

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,15 @@ Rails.application.routes.draw do
   resources :passwords, param: :token
 
   get "app", to: "dashboard#show"
-  scope "app" do
-    resources :tables do
-      post :switch, on: :member
-      resources :game_formats
-      resources :players
-      resources :sessions
-    end
+
+  # Table routes with cleaner URLs
+  post "app/:id/switch", to: "tables#switch", as: :switch_table
+  resources :tables, path: "app", only: [ :new, :create, :edit, :update, :destroy ]
+
+  # Scoped routes under the current table
+  scope "app/:table_id", as: "table" do
+    resources :game_formats
+    resources :players
+    resources :sessions
   end
 end
