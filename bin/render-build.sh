69f0bd7601498006fd9548ee7e8bee489c2b5772
Tahseen K. Kakar <tahseen.khan.kakar@gmail.com>
2024-12-22 16:22:52 +0500

    Update render-build script to include database creation step

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,5 +9,5 @@ bundle exec rails assets:clean
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
-
+bundle exec rails db:create
 bundle exec rails db:migrate
