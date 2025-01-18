ea451b16f7da41079c06463654c5cd1dd500e7eb
Tahseen K. Kakar <tahseen.khan.kakar@gmail.com>
2025-01-18 21:21:03 +0500

    Remove obsolete migration files for user, session, table, player, game format, and transaction schemas to streamline the database structure. This cleanup enhances maintainability and prepares for future schema updates.

--- db/migrate/20241221112856_add_name_and_username_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddNameAndUsernameToUsers < ActiveRecord::Migration[8.0]
-  def change
-    add_column :users, :name, :string, null: false
-    add_column :users, :username, :string, null: false
-
-    add_index :users, :username, unique: true
-  end
-end
--- db/migrate/20241222142736_add_wallet_balance_to_players.rb
+++
@@ -1,5 +0,0 @@
-class AddWalletBalanceToPlayers < ActiveRecord::Migration[8.0]
-  def change
-    add_column :players, :wallet_balance, :decimal, precision: 10, scale: 2, default: 0.0, null: false
-  end
-end
